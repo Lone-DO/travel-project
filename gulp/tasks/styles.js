d237dc8a8a21d147b4fffd7c6f9ad495662182a2
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-17 05:16:10 -0400

    Typography finishing touches, Src code prettier

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,19 @@
 const gulp = require("gulp"),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars'),
-    nestedcss = require('postcss-nested'),
-    postcssImport = require('postcss-import'),
-    mixins = require('postcss-mixins');
+   postcss = require("gulp-postcss"),
+   autoprefixer = require("autoprefixer"),
+   cssvars = require("postcss-simple-vars"),
+   nestedcss = require("postcss-nested"),
+   postcssImport = require("postcss-import"),
+   mixins = require("postcss-mixins");
 
-gulp.task("styles", function(){
-    console.log("Compiling CSS...");
-    return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([postcssImport, mixins, cssvars, nestedcss, autoprefixer]))
-        .on('error', function(errorInfo) {
-            this.emit('end');
-            console.warn(errorInfo.toString());
-        })
-        .pipe(gulp.dest('./app/temp/styles'));
+gulp.task("styles", function() {
+   console.log("Compiling CSS...");
+   return gulp
+      .src("./app/assets/styles/styles.css")
+      .pipe(postcss([postcssImport, mixins, cssvars, nestedcss, autoprefixer]))
+      .on("error", function(errorInfo) {
+         this.emit("end");
+         console.warn(errorInfo.toString());
+      })
+      .pipe(gulp.dest("./app/temp/styles"));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,31 +1,30 @@
 const gulp = require("gulp"),
-    watch = require('gulp-watch'),
-    browswerSync = require('browser-sync').create();
+   watch = require("gulp-watch"),
+   browswerSync = require("browser-sync").create();
 
-gulp.task('watch', function () {
-    browswerSync.init({
-        notify: false,
-        //open: false,
-        server: {
-            baseDir: "app"
-        }
-    });
+gulp.task("watch", function() {
+   browswerSync.init({
+      notify: false,
+      //open: false,
+      server: {
+         baseDir: "app"
+      }
+   });
 
-    watch('./app/index.html', function() {
-        console.info("HTML Changes detected...");
-        browswerSync.reload();
-    });
+   watch("./app/index.html", function() {
+      console.info("HTML Changes detected...");
+      browswerSync.reload();
+   });
 
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('cssInject');
-    });
+   watch("./app/assets/styles/**/*.css", function() {
+      gulp.start("cssInject");
+   });
 
-    watch('./app/assets/styles/**/*.postcss', function() {
-        gulp.start('cssInject');
-    });
+   watch("./app/assets/styles/**/*.postcss", function() {
+      gulp.start("cssInject");
+   });
 });
 
-gulp.task('cssInject', ['styles'], function() {
-    gulp.src('./app/temp/styles/styles.css')
-        .pipe(browswerSync.stream());
+gulp.task("cssInject", ["styles"], function() {
+   gulp.src("./app/temp/styles/styles.css").pipe(browswerSync.stream());
 });
