fcfbc35179198c7948eea3bce32d1c4917214a7a
lone-DO <lotussythe@gmail.com>
2018-03-21 05:25:00 -0400

    Sprite Template Automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,16 +1,19 @@
 const gulp = require("gulp"),
    svgSprite = require("gulp-svg-sprite"),
-   config = {
-      mode: {
-         css: {
-            render: {
-               css: {
-                  template: "./gulp/templates/sprites.css"
-               }
+   rename = require("gulp-rename");
+
+const config = {
+   mode: {
+      css: {
+         sprite: "sprite.svg",
+         render: {
+            css: {
+               template: "./gulp/templates/sprites.css"
             }
          }
       }
-   };
+   }
+};
 
 gulp.task("createSprite", function() {
    return gulp
@@ -18,3 +21,18 @@ gulp.task("createSprite", function() {
       .pipe(svgSprite(config))
       .pipe(gulp.dest("./app/temp/sprite/"));
 });
+
+gulp.task("copySpriteGraphic", ["createSprite"], function() {
+   return gulp
+      .src("./app/temp/sprite/css/**/*.svg")
+      .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprite"], function() {
+   return gulp
+      .src("./app/temp/sprite/css/*.css")
+      .pipe(rename("_sprite.css"))
+      .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("icons", ["createSprite", "copySpriteGraphic", "copySpriteCSS"]);
