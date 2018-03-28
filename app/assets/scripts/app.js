87815d4accebbe02dd4602dda04690d2dd2915a0
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-27 23:21:04 -0400

    create sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,10 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScoll";
+import StickyHeader from "./modules/StickyHeader";
 import $ from "jquery";
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
+let stickyHeader = new StickyHeader();
