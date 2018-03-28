c6b2c063b087e5c45c4c5d429bb64f723084e754
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-27 22:40:42 -0400

    create/ install waypoints

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,5 @@
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScoll";
 
 let mobileMenu = new MobileMenu();
+let revealOnScoll = new RevealOnScroll();
