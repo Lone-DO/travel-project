ca2b7eb8bd746570951d16657bc38e977896d084
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-28 13:43:59 -0400

    Completed Modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,9 +2,11 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScoll";
 import StickyHeader from "./modules/StickyHeader";
 import $ from "jquery";
+import Modal from "./modules/Modal";
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-
 let stickyHeader = new StickyHeader();
+
+let modal = new Modal();
