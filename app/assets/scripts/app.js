0de8c4815823d4e2600c9b5c5a5eaed80a27849b
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-27 21:05:02 -0400

    Settting up Babel for ES6

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,16 @@
-var Person = require("./modules/Person");
 var $ = require("jquery");
+import Person from "./modules/Person";
+
+class Adult extends Person {
+   payTaxes() {
+      console.log(this.name + " now owes $0 in taxes.");
+   }
+}
 
 alert("l;ksdjfa;sldfkj");
 
 let john = new Person("John Doe", "blue");
 john.greet();
-let jane = new Person("Jane Smith", "green");
+let jane = new Adult("Jane Smith", "orange");
 jane.greet();
+jane.payTaxes();
