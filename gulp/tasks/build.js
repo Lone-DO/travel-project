195d36c6f5f71e4a619c004e7132d9b1caa15c34
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-28 21:04:52 -0400

    Completed Build optimization

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,41 @@
 let gulp = require("gulp"),
    imgMin = require("gulp-imagemin"),
    del = require("del"),
-   usemin = require("gulp-usemin");
+   usemin = require("gulp-usemin"),
+   uglify = require("gulp-uglify"),
+   rev = require("gulp-rev"),
+   cssnano = require("gulp-cssnano"),
+   browswerSync = require("browser-sync").create();
+
+gulp.task("previewDist", function() {
+   browswerSync.init({
+      notify: false,
+      //open: false,
+      server: {
+         baseDir: "dist"
+      }
+   });
+});
 
 gulp.task("deleteDistFolder", function() {
    return del("./dist");
 });
 
-gulp.task("optimizeImages", ["deleteDistFolder"], function() {
+gulp.task("copyGeneralFiles", ["deleteDistFolder"], function() {
+   let pathsToCopy = [
+      "./app/**/*",
+      "!./app/index.html",
+      "!./app/assets/images/**",
+      "!./app/assets/styles/**",
+      "!./app/assets/scripts/**",
+      "!./app/temp",
+      "!./app/temp/**"
+   ];
+
+   return gulp.src(pathsToCopy).pipe(gulp.dest(""));
+});
+
+gulp.task("optimizeImages", ["deleteDistFolder", "icons"], function() {
    return gulp
       .src([
          "./app/assets/images/**/*",
@@ -24,11 +52,35 @@ gulp.task("optimizeImages", ["deleteDistFolder"], function() {
       .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task("usemin", ["deleteDistFolder"], function() {
+gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function() {
    return gulp
       .src("./app/index.html")
-      .pipe(usemin())
+      .pipe(
+         usemin({
+            css: [
+               function() {
+                  return rev();
+               },
+               function() {
+                  return cssnano();
+               }
+            ],
+            js: [
+               function() {
+                  return rev();
+               },
+               function() {
+                  return uglify();
+               }
+            ]
+         })
+      )
       .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("build", ["deleteDistFolder", "optimizeImages", "usemin"]);
+gulp.task("build", [
+   "deleteDistFolder",
+   "copyGeneralFiles",
+   "optimizeImages",
+   "usemin"
+]);
